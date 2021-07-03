1b519a0c05546843f332c0cbee398501f5a650c3
albertoortiz <alberto.ortiz.ponce@gmail.com>
2021-07-02 22:17:08 -0700

    segundo commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import "../topbar/topbar.scss"
 //import {} from '@material-ui/core'
 import {Person , Mail} from '@material-ui/icons'
 
-export default function Topbar() {
+export default function Topbar({menuOpen , setMenuOpen}) {
     return (
-        <div className="topbar" >
+        <div className={"topbar " + (menuOpen && 'active')} >
           <div className="wrapper" >
             <div className="left" >
                 <a href="#intro" className="logo" >Albert</a>
@@ -19,7 +19,11 @@ export default function Topbar() {
             </div>
      
             <div className="right">
-
+                <div className="hamburguer" onClick={()=>setMenuOpen(!menuOpen)} > 
+                  <span className="line1" ></span>
+                  <span className="line2" ></span>
+                  <span className="line3" ></span>
+                </div>
             </div>
           </div>
         </div>
